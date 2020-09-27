fcbf754daddb48fca88291eeb65c61ba6175da17
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2020-09-27 20:28:14 +0530

    Initial Commit

---
+++ currency-rate-service/src/test/java/lk/rc/ms/currencyrateservice/CurrencyRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.rc.ms.currencyrateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
