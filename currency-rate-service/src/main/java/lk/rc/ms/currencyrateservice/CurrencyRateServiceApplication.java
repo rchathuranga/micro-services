b1a358c32e25239fab427f794ca4a834ad0958d0
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2020-10-02 22:59:58 +0530

    Configure Eureka Server in Micro Services

--- currency-rate-service/src/main/java/lk/rc/ms/currencyrateservice/CurrencyRateServiceApplication.java
+++ currency-rate-service/src/main/java/lk/rc/ms/currencyrateservice/CurrencyRateServiceApplication.java
@@ -2,8 +2,10 @@ package lk.rc.ms.currencyrateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyRateServiceApplication {
 
     public static void main(String[] args) {
