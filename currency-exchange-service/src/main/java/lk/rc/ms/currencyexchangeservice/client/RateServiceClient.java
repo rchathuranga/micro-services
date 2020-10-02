42a63dd36f7f4b8f1ef1777c6ce5b38b4970341f
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2020-10-02 22:24:49 +0530

    Implement Ribbon Client For Load Balancing

--- currency-exchange-service/src/main/java/lk/rc/ms/currencyexchangeservice/client/RateServiceClient.java
+++ currency-exchange-service/src/main/java/lk/rc/ms/currencyexchangeservice/client/RateServiceClient.java
@@ -1,14 +1,17 @@
 package lk.rc.ms.currencyexchangeservice.client;
 
 import lk.rc.ms.currencyexchangeservice.bean.CurrencyRateBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-rate-service", url = "http://localhost:8001/crs")
+//@FeignClient(name = "currency-rate-service", url = "http://localhost:8001/crs")
+@FeignClient(name = "currency-rate-service")
+@RibbonClient(name = "currency-rate-service")
 public interface RateServiceClient {
 
-    @GetMapping(value = "/get-rate/fromCurrency/{fromCurrency}/toCurrency/{toCurrency}")
+    @GetMapping(value = "/crs/get-rate/fromCurrency/{fromCurrency}/toCurrency/{toCurrency}")
     public CurrencyRateBean getRate(@PathVariable String fromCurrency, @PathVariable String toCurrency) throws Exception;
 
 }
